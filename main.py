2454c9e438c931e32ae4b0f27ff9ba0da37a62e3
sayano <sayano.lee@gmail.com>
2018-12-19 17:53:19 +0800

    files update, submodule imported

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from os.path import join as opj
 from model import Extractor
 
 from torch.autograd import Variable
+from torchvision.datasets import ImageFolder
+from torchvision import transforms
 
 from tqdm import tqdm
 from argparse import ArgumentParser
@@ -22,6 +24,7 @@ parser.add_argument("--gpu_list", type=str, default="0", help="which gpus used f
 parser.add_argument("--train_path", type=str, default="./data/icdar2015/train")
 parser.add_argument("--test_path", type=str, default="./data/icdar2015/test")
 
+parser.add_argument("--icdar_patches", type=str, default="./data/icdar2015/patches")
 parser.add_argument("--icdar_true_path", type=str, default="./data/icdar2015/patches/1")
 parser.add_argument("--icdar_false_path", type=str, default="./data/icdar2015/patches/0")
 
@@ -32,10 +35,7 @@ gpus = list(range(len(args.gpu_list.split(','))))
 
 def make_dataset():
 
-    batch_size = args.batch_size_per_gpu * len(gpus)
-
     icdar_loader = icdar(args.train_path)
-    model = Extractor()
 
     pbar = tqdm(total=len(icdar_loader))
     true_count = 0
@@ -56,5 +56,25 @@ def make_dataset():
     pbar.close()
 
 
+def extract(loader, model):
+    for cnt, (im, ann) in enumerate(loader):
+
+        feat = model(im)
+        import ipdb
+        ipdb.set_trace()
+
+
 if __name__ == '__main__':
-    make_dataset()
+    # Step 1 : extract bboxes
+    # make_dataset()
+
+    # Step 2 : extract features
+    transforms = transforms.Compose([transforms.Resize((224,224)),
+                                     transforms.ToTensor(),
+                                     transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                          std=[0.229,0.224,0.225])])
+    dataset = ImageFolder(root=args.icdar_patches, transform=transforms)
+    loader = torch.utils.data.DataLoader(dataset, batch_size=10, shuffle=True)
+
+    model = Extractor()
+    extract(loader=loader, model=model)
--- model.py
+++ model.py
@@ -6,14 +6,13 @@ class Extractor(nn.Module):
     def __init__(self):
         super(Extractor, self).__init__()
         model = models.resnet50(pretrained=True)
-        self.model = nn.Sequential(*list(model.children())[:-2])
-        self.pool = nn.AvgPool2d(kernel_size=16)
+        self.model = nn.Sequential(*list(model.children())[:-1])
+        # self.pool = nn.AvgPool2d(kernel_size=16)
 
     def forward(self, input):
         x = self.model(input)
-        x = self.pool(x)
+        # x = self.pool(x)
         return x.squeeze()
-
 
 if __name__ == '__main__':
     pass

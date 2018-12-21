7c2763e530402b3c56533653923e3c914f802365
sayano <sayano.lee@gmail.com>
2018-12-21 08:29:14 +0800

    files update

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ from os.path import join as opj
 from model import Extractor
 
 from torch.autograd import Variable
-from torchvision.datasets import ImageFolder
 from torchvision import transforms
 
 from tqdm import tqdm
 from argparse import ArgumentParser
+import shutil
 
 from icdar import icdar
-
+from folder import ImageFolder
 from CPMMC_python import CPMMC
 
 parser = ArgumentParser()
@@ -29,6 +29,9 @@ parser.add_argument("--test_path", type=str, default="./data/icdar2015/test")
 parser.add_argument("--icdar_patches", type=str, default="./data/icdar2015/patches")
 parser.add_argument("--icdar_true_path", type=str, default="./data/icdar2015/patches/1")
 parser.add_argument("--icdar_false_path", type=str, default="./data/icdar2015/patches/0")
+
+parser.add_argument("--split_path_0", type=str, default="./tmp/result/0")
+parser.add_argument("--split_path_1", type=str, default="./tmp/result/1")
 
 args = parser.parse_args()
 
@@ -58,19 +61,59 @@ def make_dataset():
     pbar.close()
 
 
-def extract(loader, model):
-    for cnt, (im, ann) in enumerate(loader):
+def clustering(loader, model):
 
-        feat = model(im)
-        import ipdb
-        ipdb.set_trace()
+    print("=======> clustering")
+    pbar = tqdm(total=len(loader))
+
+    #convert into (1, 1) numpy array for further deployment
+    C = np.array([[0.01]])
+    l = np.array([[10]])
+    b_0 = np.array([[0]])
+    xi_0 = np.array([[0.5]])
+
+    epsilon = 0.1
+
+    MMC = CPMMC.CPMMC(dim=2048, C=C, \
+                      epsilon=epsilon, \
+                      l=l,b_0=b_0,xi_0=xi_0)
+
+    cls0_path = args.split_path_0
+    cls1_path = args.split_path_1
+
+    model = model.cuda()
+
+    for cnt, (data, label, path) in enumerate(loader):
+
+        pbar.update(1)
+
+        data = data.cuda()
+        feat = model(data)
+        feat = feat.cpu()
+
+        data = feat.numpy()
+        new_label = -np.ones(label.shape, dtype=np.int8)
+        for count, num in enumerate(label):
+            if num == 1:
+                new_label[count] = 1
+
+        acc, pred = MMC(data=data, label=new_label)
+
+        for cnt, flag in enumerate(pred):
+            if flag.item() == 1:
+                shutil.copy(path[cnt], cls0_path)
+            if flag.item() == -1:
+                shutil.copy(path[cnt], cls1_path)
+
+    pbar.close()
+    return acc
 
 
 if __name__ == '__main__':
     # Step 1 : extract bboxes
-    make_dataset()
+    #make_dataset()
 
-    # Step 2 : extract features
+    # Step 2 : extract features and clustering
     transforms = transforms.Compose([transforms.Resize((224,224)),
                                      transforms.ToTensor(),
                                      transforms.Normalize(mean=[0.485, 0.456, 0.406],
@@ -79,4 +122,6 @@ if __name__ == '__main__':
     loader = torch.utils.data.DataLoader(dataset, batch_size=10, shuffle=True)
 
     model = Extractor()
-    extract(loader=loader, model=model)
+    for params in model.parameters():
+        params.requires_grad = False
+    acc = clustering(loader=loader, model=model)
